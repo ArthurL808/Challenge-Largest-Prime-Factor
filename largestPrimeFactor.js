1dd00f834b68475981bfabff78620d39a3a8b304
Arthur <kingart808@gmail.com>
2019-07-20 10:35:42 -1000

    completed

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,10 +1,8 @@
 exports.largestPrimeFactor = function(n) {
   let primeNumber = 0;
-//do your work here
-for (let i = 2; i < primeNumber; i++) {
-  if(i % primeNumber == 0){
-
+  for (let i = 2; i < primeNumber; i++) {
+    if (i % primeNumber == 0) {
+    }
   }
-}
   return primeNumber;
 };
